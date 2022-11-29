40555c842cb80c379ce0c3e595b9d9e3166dd140
Denys Shapoval <96021986+dawmsi@users.noreply.github.com>
2022-11-29 21:16:40 +0200

    autohide

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-let mobileMenu = document.querySelector(".nav-mobile-menu");
-let mainMenu = document.querySelector(".navigation")
+let mobileMenu = document.querySelector('.nav-mobile-menu')
+let mainMenu = document.querySelector('.navigation')
+const navLinks = document.querySelectorAll('.navLink')
 
+const OpenNavBar = () => {
+    mobileMenu.classList.toggle('active-menu')
+    if (mobileMenu.classList.contains('active-menu')) {
+        mainMenu.classList.add('active-menu')
+    } else {
+        mainMenu.classList.remove('active-menu')
+    }
+}
 
-mobileMenu.addEventListener("click", function () {
-    mobileMenu.classList.toggle("active-menu");
-    if (mobileMenu.classList.contains("active-menu")) {
-        mainMenu.classList.add("active-menu");
-      } else {
-        mainMenu.classList.remove("active-menu");
-      }
-  });
+mobileMenu.addEventListener('click', OpenNavBar, { passive: true })
+
+const loop = navLinks.forEach((link) => {
+    link.addEventListener('click', OpenNavBar, { passive: true })
+})
